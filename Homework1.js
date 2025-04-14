1351b2885ca18a6a7ed8fe49399b30d1eca4f8a7
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2025-04-14 14:38:55 +0300

    added tasks

---
+++ Homework1.js
@@ -0,0 +1,82 @@
+/* task 2
+
+let lastName = "Maliushchenko";
+console.log(lastName); */
+
+
+/* task 3
+let a;
+let b;
+
+a = "Hello";
+b = 42;
+
+console.log("a =", a);
+console.log("b =", b);
+
+b = a;
+
+console.log("a =", a);
+console.log("b =", b); */
+
+
+/* task 4
+let user = new Object;
+user = {
+    String: "Alena",
+    Number: 18,
+    Boolean: false,
+    Undefined: undefined,
+    Null: null
+}; */
+
+
+/* task 5
+let age = prompt('How old are you?');
+let isAdult = age >= 18;
+console.log("isAdult =", isAdult);
+*/
+
+
+/* task 6
+let firstName = "Alena";
+let lastName = "Maliushchenko";
+let group = 130;
+let year = 1989;
+let isMarried = true;
+
+let someNull = null;
+if (someNull === null){
+    console.log("Дійсно null");
+}
+let someUndefined;
+
+console.log(typeof group);
+console.log(typeof year);
+
+console.log(typeof isMarried);
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+
+console.log(typeof someNull); // виводить об'єкт
+console.log(typeof someUndefined); */
+
+
+/*  task 7
+let login = prompt('Write your login', 'AlenaM');
+let mail = prompt('Write your mail', 'alenamaliushchenko@gmail.com');
+let password = prompt('Write you password', 'dghj14');
+
+alert(`Dear ${login}, your email is ${mail}, your password is ${password}`);
+ */
+
+/* task 8
+let secondsInHour = 60 * 60;
+let secondsInDay = secondsInHour * 24;
+let secondsInMonth = secondsInDay * 30;
+
+console.log('Кількість секунд в годині:', secondsInHour);
+console.log('Кількість секунд в дні:', secondsInDay);
+console.log('Кількість секунд в місяці:', secondsInMonth);
+ */
